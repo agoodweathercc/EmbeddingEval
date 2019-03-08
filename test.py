7482b4b261b1973561d95e06b5969a54317c47b0
Chen Cai <agoodweathercc>
2019-03-07 21:16:50 -0500

    add test.py

--- test.py
+++ test.py
@@ -1,51 +1,164 @@
-import matplotlib.pyplot as plt
+# https://github.com/keras-team/keras/issues/4613
+import sys
+import os
+import json
+import networkx as nx
+import tensorflow as tf
 from time import time
+import numpy as np
+import argparse
 
-from gem.utils      import graph_util, plot_util
-from gem.evaluation import visualize_embedding as viz
-from gem.evaluation import evaluate_graph_reconstruction as gr
+sys.path.append('/home/cai.507/Documents/DeepLearning/EmbeddingEval/node_classification/NRL/src/')
+import classification
+# sys.path.append('/home/cai.507/Documents/DeepLearning/EmbeddingEval/NRL/src/')
+sys.path.append('/home/cai.507/Documents/DeepLearning/deep-persistence/pythoncode/GEM/')
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
+np.random.seed(1337)
 
-from gem.embedding.gf       import GraphFactorization
-from gem.embedding.hope     import HOPE
-from gem.embedding.lap      import LaplacianEigenmaps
-from gem.embedding.lle      import LocallyLinearEmbedding
-from gem.embedding.node2vec import node2vec
+from gem.utils      import graph_util
+from gem.utils.logutil      import set_logger, make_direct
+from gem.evaluation import evaluate_node_classification as nc
+from gem.embedding.lap import LaplacianEigenmaps
 from gem.embedding.sdne     import SDNE
+from gem.embedding.sdne_utils import model_batch_predictor
+from aux import batch_generator_sdne_modified, batch_generator_sdne, get_search_num, get_one_hyper, make_hyper_direct
 
+def load_hyperparameter(method='lap', dataset = 'karate'):
+    # method can be ['node2vec', 'sdne', 'gf', 'hope', 'lap', 'lle']
+    # hyper_dict = open('./blogcat_hypRange_1.txt').read()
+    # hyper_dict = open('./blogcat_hypRange_1.txt').read()
+    hyper_dict = open('./parameters.txt').read()
+    import ast
+    hyper_dict = ast.literal_eval(hyper_dict)
+    if dataset in ['texas', 'cornell', 'wisconsin', 'washington']:
+        hyper_dict['sdne']['n_batch'] = [50]
+    if dataset in ['flickr', 'microsoft', 'p2p-gnutella31']:
+        # hyper_dict['sdne']['n_iter'] = [[5], [10]]
+        hyper_dict['sdne']['n_iter'] = [[5]]
+    if dataset in ['wikipedia']:
+        hyper_dict['sdne']['n_iter'] = [[50]]
+    if dataset in ['blogcatalog']:
+        hyper_dict['sdne']['n_iter'] = [[50],[20]]
+        hyper_dict['sdne']['alpha'] = [1e-05]
+        hyper_dict['sdne']['beta'] = [5]
 
-# File that contains the edges. Format: source target
-# Optionally, you can add weights as third column: source target weight
-edge_f = 'gem/data/TEST_50M.edgelist'
-# Specify whether the edges are directed
-isDirected = True
+    return hyper_dict[method]
 
-# Load graph
-G = graph_util.loadGraphFromEdgeListTxt(edge_f, directed=isDirected)
-G = G.to_directed()
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', default='wikipedia', help="load from data/")
+parser.add_argument('--method', default='sdne', help='node2vec, sdne, gf, hope, lap, lle')
+parser.add_argument('--task', default='nc', help="nc or lp")
+parser.add_argument('--fold', default='0', help="0, 1, 2, 3, 4")
+parser.add_argument('--newloss', default=False)
 
-models = []
-# You can comment out the methods you don't want to run
-models.append(GraphFactorization(d=2, max_iter=50000, eta=1 * 10**-4, regu=1.0))
-models.append(HOPE(d=4, beta=0.01))
-models.append(LaplacianEigenmaps(d=2))
-models.append(LocallyLinearEmbedding(d=2))
-models.append(node2vec(d=2, max_iter=1, walk_len=80, num_walks=10, con_size=10, ret_p=1, inout_p=1))
-models.append(SDNE(d=2, beta=5, alpha=1e-5, nu1=1e-6, nu2=1e-6, K=3,n_units=[50, 15,], rho=0.3, n_iter=50, xeta=0.01,n_batch=500,
-                modelfile=['./intermediate/enc_model.json', './intermediate/dec_model.json'],
-                weightfile=['./intermediate/enc_weights.hdf5', './intermediate/dec_weights.hdf5']))
+if __name__ == '__main__':
+    # sys.argv = ['--dataset==co-author']
+    args = parser.parse_args()
+    method = args.method
+    method2func = {'sdne': 'SDNE', 'node2vec': 'node2vec',
+                   'gf': 'GraphFactorization', 'hope':'HOPE',
+                   'lap':'LaplacianEigenmaps', 'lle': 'LocallyLinearEmbedding'}
+    dataset = args.dataset
+    edge_f, labels_f, mat_f = './data/' + dataset + '.edgelist', './data/' + dataset + '.npy', './data/' + dataset + '.mat'
+    isDirected = True if dataset in ['co-author', 'pubmed', 'wikivote'] else False
 
+    # Load graph
+    if args.task == 'nc':
+        G = graph_util.loadGraphFromEdgeListTxt(edge_f, directed=isDirected)
+    elif args.task == 'lp':
+        G = graph_util.loadGraphForLP(args)
+        if isDirected: G = G.to_directed()
+    else:
+        raise Exception('No such task')
 
-for embedding in models:
-    print ('Num nodes: %d, num edges: %d' % (G.number_of_nodes(), G.number_of_edges()))
-    t1 = time()
-    # Learn embedding - accepts a networkx graph or file with edge list
-    Y, t = embedding.learn_embedding(graph=G, edge_f=None, is_weighted=True, no_python=True)
-    print (embedding._method_name+':\n\tTraining time: %f' % (time() - t1))
-    # Evaluate on graph reconstruction
-    MAP, prec_curv = gr.evaluateStaticGraphReconstruction(G, embedding, Y, None)
-    #---------------------------------------------------------------------------------
-    print(("\tMAP: {} \t preccision curve: {}\n\n\n\n"+'-'*100).format(MAP,prec_curv))
-    #---------------------------------------------------------------------------------
-    # Visualize
-    viz.plot_embedding2D(embedding.get_embedding(), di_graph=G, node_colors=None)
-    plt.show()
+    print(nx.info(G))
+    n_nodes = len(G)
+    print('finish converting...')
+    if dataset != 'karate': labels = np.load(labels_f)
+
+    searchrange = load_hyperparameter(method = method, dataset=dataset) # a dict
+    n_models = get_search_num(searchrange)[0]
+    models = []
+    for idx in range(n_models):
+        hyper = get_one_hyper(searchrange, dataset, idx=idx, method=method)
+        make_hyper_direct(method=method, dataset=dataset, make_flag=True, **hyper)
+        models.append(eval(method2func[method])(**hyper))
+    print('finish easy work')
+
+    for embedding in models:
+        t1 = time()
+        direct = embedding.__getattribute__('_direct')
+        make_direct(direct)
+        logger = set_logger(os.path.join(direct, 'train_' + args.task + '.log'), reset_flag=True)
+        logger.info('Start Training...')
+        logger.info('directory is %s' %direct)
+        run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+        run_metadata = tf.RunMetadata()
+        name = 'fold_' + args.fold if args.task == 'lp' else 'emb_nc'
+
+        # if emb already exists, then skip
+        if os.path.exists(direct + name + '.npy'):
+            print('The following already exist')
+            print(direct + name)
+            continue
+
+        if True: # generate embedding or load existing embedding
+            if method == 'sdne':
+                self = embedding.learn_embedding(graph=G, edge_f=None, is_weighted=True, no_python=True)
+                model = self._model
+                print('finish getting self')
+                S = nx.to_scipy_sparse_matrix(G)
+
+                my_generator = batch_generator_sdne_modified(S, n_nodes, searchrange['beta'][0], searchrange['n_batch'][0], True)
+                my_generator_ = batch_generator_sdne(S, n_nodes, searchrange['beta'][0], searchrange['n_batch'][0], True)
+                print('finish setting generator')
+
+                history_callback = model.fit_generator(
+                    my_generator_,
+                    nb_epoch= searchrange['n_iter'][0][0],
+                    samples_per_epoch=n_nodes // searchrange['n_batch'][0],
+                    validation_data=None, class_weight=None, nb_worker=10,
+                    verbose=1, max_queue_size = 5)
+                Y, t = model_batch_predictor(self._autoencoder, S, self._n_batch), 0
+
+                for epoch in history_callback.epoch:
+                    for key, val in history_callback.history.items():
+                        logger.info(str(epoch) + ' ' + str(key) + ': ' + str(val[epoch])),
+            else:
+                Y, t = embedding.learn_embedding(graph=G, edge_f=None, is_weighted=True, no_python=True, logger=logger)
+
+            # save embedding
+            name = 'fold_' + args.fold  if args.task == 'lp' else 'emb_nc'
+            np.save(direct + name, Y)
+            # np.savetxt(direct + 'embedding_.txt', Y)
+            logger.info (embedding._method_name+':\n\tTraining time: %f, save file at %s' % (time() - t1, direct+name))
+            logger.info('Finish evaluating static graph reconstruction\n\n\n')
+
+            # save param
+            with open(direct+ 'param_' + args.task + '.json', 'w') as fp:
+                json.dump(embedding.__getattribute__('_param'), fp, indent=1)
+            continue
+
+        # code that is no longer being used
+        try:
+            training_percents = np.linspace(0.1, 0.9, 9) if dataset not in ['flickr', 'youtube','co-author'] else np.linspace(0.01, 0.10, 10)
+            training_percents = list(training_percents)
+            print(direct + name)
+            logger.info(classification.classify(emb=direct + name + '.npy', network=mat_f, writetofile=True, classifier='LR',
+                                        dataset=dataset, algorithm=method, word2vec_format=False,
+                                        num_shuffles=5, output_dir=direct,
+                                        training_percents=training_percents))
+            #
+            # logger.info(classification.classify(emb=direct + name + '.npy', network=mat_f, writetofile=True, classifier='EigenPro',
+            #                             test_kernel="eigenpro",
+            #                             dataset=dataset, algorithm=method, word2vec_format=False,
+            #                             num_shuffles=5, output_dir=direct,
+            #                             training_percents=training_percents))
+            sys.exit()
+
+        except:
+            logger.info('Evaluation Error')
+        continue
+#######################################################################################################################
+
